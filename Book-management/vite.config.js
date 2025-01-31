685dbb14d1dc249fe316a26d65af32f0e1b719d0
Pareekpriya <priyapareek754@gmail.com>
2025-01-31 14:30:33 +0530

    base changes

--- Book-management/vite.config.js
+++ Book-management/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/BookStore_management",
+  base:"/BookStore_management/Book-management",
   plugins: [react()],
 })
